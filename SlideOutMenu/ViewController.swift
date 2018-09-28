9e0bc98b3ac6bf4f3b9c7911c518c1f7bb761a03
Rickey Hrabowskie <rjhrabowskie@gmail.com>
2018-09-28 18:51:52 -0400

    Initial Commit

---
+++ SlideOutMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOutMenu
+//
+//  Created by Rickey Hrabowskie on 9/28/18.
+//  Copyright © 2018 Rickey Hrabowskie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
